96769175860d6deb119176860b5c5033656e16a2
xofaye <faye.feng26@gmail.com>
2016-11-30 21:12:22 -0500

    Change source of port

--- app.js
+++ app.js
@@ -72,5 +72,6 @@ app.use(function(req, res, next) {
 
 module.exports = app;
 
-app.listen(3000);
-console.log('Listening on port 3000');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
